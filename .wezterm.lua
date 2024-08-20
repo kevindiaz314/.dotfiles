518c69763f8e65368227bccbaf192380ff698fae
Kevin <kevin@kevinmbp.mynetworksettings.com>
2024-08-19 21:29:10 -0400

    Increase font size to 16

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 15
+config.font_size = 16
 
 config.window_decorations = "RESIZE"
 
