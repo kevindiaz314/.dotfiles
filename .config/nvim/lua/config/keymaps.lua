24a61e9ae870cb0e16c6f6a9562a598069f86699
Kevin <kevin@kevinmbp.mynetworksettings.com>
2024-08-21 23:29:40 -0400

    Add key mappings for search clearing and window management

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,15 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-local keymap = vim.keymap -- for conciseness
+-- Local variable to simplify key mapping configurations
+local keymap = vim.keymap 
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+keymap.set("n", "<leader>ur", "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>", { desc = "Redraw / Clear hlsearch / Diff Update" })
+
+-- Window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
