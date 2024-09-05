bc62c17b9574e9b07d9cee00fcd5ff90c089a444
Kevin <kevin@kevinmbp.mynetworksettings.com>
2024-09-05 14:24:56 -0400

    Add file_browser extension and some keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,9 @@
 return { "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    lazy = false,
+    event = 'VeryLazy',   
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-file-browser.nvim",
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'gmake' },
         "nvim-tree/nvim-web-devicons",
     },
@@ -10,6 +11,8 @@ return { "nvim-telescope/telescope.nvim",
         -- To get fzf loaded and working with telescope, you need to call
         -- load_extension, somewhere after setup function:
         require('telescope').load_extension('fzf')
+
+        require('telescope').load_extension('file_browser')
     end,
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Lists files in your cwd" } },
@@ -17,5 +20,7 @@ return { "nvim-telescope/telescope.nvim",
         { "<leader>fb", "<cmd>Telescope buffers<cr>" , { desc = "Lists open buffers in current Neovim instance" } },
         { "<leader>fc", "<cmd>Telescope grep_string<cr>" , { desc = "Searches for the string under your cursor" } },
         { "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Lists previously open files" } },
+        { "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy search through the output of git ls-files" } },
+        { "<leader>fe", "<cmd>Telescope file_browser<cr>", { desc = "" } },
     }
 }
