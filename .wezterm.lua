611e544b569effa407b067efdf79ac38b8c41d42
Kevin <kevin@kevinmbp.mynetworksettings.com>
2024-09-04 22:42:09 -0400

    Decrease font size to 15 and delete font since Wezterm already uses JetBrains Mono by default

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,8 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 16
+config.font_size = 15
 
 config.window_decorations = "RESIZE"
 
@@ -13,7 +12,7 @@ config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
 -- Color scheme
-config.color_scheme = 'Kanagawa (Gogh)'
+config.color_scheme = 'kanagawa (Gogh)'
 
 -- Opacity
 -- config.window_background_opacity = 0.2
